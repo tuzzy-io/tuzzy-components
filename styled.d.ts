b3613cb8420b32e6546734fefbead12563e2afff
maxtuzz <maxtuzzolino@gmail.com>
2020-06-01 14:23:28 +1200

    added default theme interface

---
+++ styled.d.ts
@@ -0,0 +1,15 @@
+export interface DefaultTheme {
+    colors: {
+        sidebarColor: string;
+        sidebarHighlight: string;
+
+        // Screens etc.
+        backgroundColor: string;
+
+        // Main component pallet
+        main: string;
+        secondary: string;
+        active: string;
+        words: string;
+    }
+}
